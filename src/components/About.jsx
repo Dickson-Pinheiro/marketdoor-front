b891dc1c87ef962bdb45f5bf5ab3f66d27607258
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-06-28 05:44:36 -0300

    refact: update layout home component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,6 @@ export default function About() {
 
     return (
         <AboutContainer>
-            <h1>Crie seu <span>mercado</span> e gerencie uma ou mais <span>lojas</span>!</h1>
             <ContainerText>
                 <p>Na Market Door, você pode <span>criar o seu perfil de mercado</span> e <span>gerenciar  suas lojas</span>.  Você possui o controle para cadastrar novas lojas e gerenciar o acesso para cada uma, dando a liberdade para que cada loja cadastre seus próprios produtos e controle o seu delivery.</p>
                 <p>Com foco no <span>gerenciamento</span> de estoque, nossa proposta é conseguir integrar sua loja física com o nosso app de delivery. Dessa forma, todas as promoções e disponibilidade de produtos estarão atualizados em tempo real, <span>sinctonizados com a sua loja.</span></p>
@@ -36,15 +35,6 @@ const AboutContainer = styled.main`
     flex-direction: column;
     align-items: center;
     gap: 30px;
-    h1 {
-        font-size: 24px;
-        font-weight: 700;
-        font-family: 'Nunito', sans-serif;
-        color: #030532;
-        span {
-            color: #FFB800;
-        }
-    }
 `
 
 const ContainerText = styled.div`
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,25 +8,49 @@ export default function Home() {
     return (
         <ContainerHome>
             <Header></Header>
-            <Fruits></Fruits>
+            <Content>
+            <h1>Crie seu <span>mercado</span> e gerencie uma ou mais <span>lojas</span>!</h1>
             <Container>
                 <About></About>
                 <ContainerCreateMarket>
-                    <h1>Crie seu cadastro como mercado aqui</h1>
                     <MarketForm></MarketForm>
                 </ContainerCreateMarket>
             </Container>
+            </Content>
         </ContainerHome>
     )
 }
 
+const Content = styled.div`
+     h1 {
+        text-align: center;
+        margin-top: 50px;
+        margin-bottom: 50px;
+        font-size: 24px;
+        font-weight: 700;
+        font-family: 'Nunito', sans-serif;
+        color: #030532;
+        span {
+            color: #FFB800;
+        }
+    }
+`
+
 const ContainerHome = styled.div`
     width: 100%;
     min-height: 100vh;
+    display: flex;
+    flex-direction: column;
 `
 
 const Container = styled.div`
     display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    max-width: 900px;
+    flex: 1;
+    margin: 0 auto;
 `
 
 const ContainerCreateMarket = styled.div`
@@ -34,6 +58,7 @@ const ContainerCreateMarket = styled.div`
     flex-direction: column;
     align-items: center;
     width: 100%;
+    margin: 0 auto;
     justify-content: center;
     gap: 24px;
 
