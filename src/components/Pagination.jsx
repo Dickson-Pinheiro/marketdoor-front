d99d85c099e1b1171eb83abe3be851d708ad5161
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-07-01 00:36:54 -0300

    feat: add arrow in pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,68 +1,83 @@
-import { useEffect } from "react"
-import { styled } from "styled-components"
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+import { styled } from 'styled-components'
 
 export default function Pagination({ current, setCurrent, count, updateProducts, setUpdateProducts }) {
 
-    function updateCurrent(newCurrent){
+    function updateCurrent(newCurrent) {
         setCurrent(newCurrent)
         setUpdateProducts(!updateProducts)
     }
-    let pageLength = 12
+    let pageLength = 4
     if (count < pageLength) {
         return ''
     }
     if (current === 1 && Math.ceil(count / pageLength) === 2) {
         return (
-            <Container>
-                <Selected>{current}</Selected>
-                <NoSelected onClick={() => updateCurrent(current + 1)}>{current + 1}</NoSelected>
-            </Container>
+            <ContainerArrow>
+                <FaArrowLeft color='#868585' />
+                <Container>
+                    <Selected>{current}</Selected>
+                    <NoSelected onClick={() => updateCurrent(current + 1)}>{current + 1}</NoSelected>
+                </Container>
+                <FaArrowRight />
+            </ContainerArrow>
         )
     }
     if (current === 1 && Math.ceil(count / pageLength) > 2) {
         return (
-            <Container>
-                <Selected>{current}</Selected>
-                <NoSelected onClick={() => updateCurrent(current + 1)}>{current + 1}</NoSelected>
-                <NoSelected onClick={() => updateCurrent(current + 2)}>{current + 2}</NoSelected>
-            </Container>
+            <ContainerArrow>
+                <FaArrowLeft color='#868585' />
+                <Container>
+                    <Selected>{current}</Selected>
+                    <NoSelected onClick={() => updateCurrent(current + 1)}>{current + 1}</NoSelected>
+                    <NoSelected onClick={() => updateCurrent(current + 2)}>{current + 2}</NoSelected>
+                </Container>
+                <FaArrowRight onClick={() => updateCurrent(current + 1)} />
+            </ContainerArrow>
         )
     }
     if (current === Math.ceil(count / pageLength) && Math.ceil(count / pageLength) === 2) {
         return (
-            <Container>
-                <NoSelected onClick={() => updateCurrent(current - 1)}>{current - 1}</NoSelected>
-                <Selected>{current}</Selected>
-            </Container>
+            <ContainerArrow>
+                <FaArrowLeft onClick={() => updateCurrent(current - 1)} />
+                <Container>
+                    <NoSelected onClick={() => updateCurrent(current - 1)}>{current - 1}</NoSelected>
+                    <Selected>{current}</Selected>
+                </Container>
+            </ContainerArrow>
         )
     }
 
     if (current > 1 && current !== Math.ceil(count / pageLength)) {
         return (
-            <Container>
-                <NoSelected onClick={() => updateCurrent(current - 1)}>{current - 1}</NoSelected>
-                <Selected>{current}</Selected>
-                <NoSelected onClick={() => updateCurrent(current + 1)}>{current + 1}</NoSelected>
-            </Container>
+            <ContainerArrow>
+                <FaArrowLeft onClick={() => updateCurrent(current - 1)} />
+                <Container>
+                    <NoSelected onClick={() => updateCurrent(current - 1)}>{current - 1}</NoSelected>
+                    <Selected>{current}</Selected>
+                    <NoSelected onClick={() => updateCurrent(current + 1)}>{current + 1}</NoSelected>
+                </Container >
+                <FaArrowRight onClick={() => updateCurrent(current + 1)} />
+            </ContainerArrow>
         )
     }
     if (current > 1 && current === Math.ceil(count / pageLength)) {
         return (
-            <Container>
-                <NoSelected onClick={() => updateCurrent(current - 2)}>{current - 2}</NoSelected>
-                <NoSelected onClick={() => updateCurrent(current - 1)}>{current - 1}</NoSelected>
-                <Selected>{current}</Selected>
-            </Container>
+            <ContainerArrow>
+                <FaArrowLeft onClick={() => updateCurrent(current - 1)} />
+                <Container>
+                    <NoSelected onClick={() => updateCurrent(current - 2)}>{current - 2}</NoSelected>
+                    <NoSelected onClick={() => updateCurrent(current - 1)}>{current - 1}</NoSelected>
+                    <Selected>{current}</Selected>
+                </Container>
+                <FaArrowRight color='#868585'/>
+            </ContainerArrow>
         )
     }
 }
 
 const Container = styled.div`
     display: flex;
-    margin: 0 auto;
-    width: 150px;
-    margin: 0 auto;
-    margin-top: 30px;
 `
 
 const NoSelected = styled.div`
@@ -76,6 +91,9 @@ const NoSelected = styled.div`
     color: #FFFFFF;
     font-family: 'Nunito', sans-serif;
     font-weight: 800;
+    &:hover {
+        background-color: rgb(245, 131, 54);
+    }
 `
 const Selected = styled.div`
     width: 30px;
@@ -88,4 +106,20 @@ const Selected = styled.div`
     color: #FFFFFF;
     font-family: 'Nunito', sans-serif;
     font-weight: 800;
+`
+const ContainerArrow = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    width: 150px;
+    margin: 0 auto;
+    margin-top: 30px;
+    svg {
+        cursor: pointer;
+        transition: 0.3s;
+        &:hover {
+            color: #FFB800;
+        }
+    }
 `
